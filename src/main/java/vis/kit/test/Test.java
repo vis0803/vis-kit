b344e96c853551833ea33a08a93fa1a6525b71ef
wyj <15144042132@163.com>
2026-05-21 14:48:09 +0800

    misc:测试

---
+++ src/main/java/vis/kit/test/Test.java
@@ -0,0 +1,9 @@
+package vis.kit.test;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("args"+ Arrays.toString(args));
+    }
+}
