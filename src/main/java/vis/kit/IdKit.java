eb3bc4c0c07a8b1547d58cd21e5a482af788312f
wyj <15144042132@163.com>
2026-05-26 11:06:24 +0800

    misc:测试id生成器

--- src/main/java/vis/kit/IdKit.java
+++ src/main/java/vis/kit/IdKit.java
@@ -4,6 +4,8 @@ import vis.kit.IdGenerator.contract.IIdGenerator;
 import vis.kit.IdGenerator.contract.IdGeneratorOptions;
 import vis.kit.IdGenerator.idgen.DefaultIdGenerator;
 
+import java.util.HashSet;
+
 /**
  * 预分配64个，workId取值 0-63 ，默认使用 0
  */
@@ -34,7 +36,33 @@ public class IdKit {
         return nextId(workId) + "";
     }
 
-    public static void main(String[] args) {
-        System.out.println(nextId());
+    private static final HashSet<Long> nowR = new HashSet<>();
+
+    private static final HashSet<Long> R = new HashSet<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        int count = 0;
+
+        for (int k = 0; k < 2000; k++) {
+            for (int i = 0; i < 60; i++) {
+                final int workerId = i;
+                Thread vt1 = Thread.startVirtualThread(() -> {
+                    long l = nextId(workerId);
+                    if (nowR.contains(l)) {
+                        System.out.println(l);
+                        R.add(l);
+                    }
+                    nowR.add(l);
+                });
+                count++;
+            }
+        }
+        System.out.println("12W 条 多线程同时执行，独自生成ID");
+        System.out.println("添加5秒延迟，等待数据结束");
+        Thread.sleep(5000);
+        System.out.println("累计生成：" + count + "条");
+        System.out.println("重复数据：" + R.size() + "条");
+        System.out.println("不重复数据：" + nowR.size() + "条");
+
     }
 }
